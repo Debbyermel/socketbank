7efb2dfa0ddddfe08d312844361610ede140e33a
Debora Ermel <debbyermel@gmail.com>
2021-08-09 16:23:31 -0500

    Load data

--- js/outline-script.js
+++ js/outline-script.js
@@ -23,3 +23,22 @@
 
 	unfocus();
 })(document, window);
+
+
+
+
+//Lecture
+//const transactions = [200, 450, -600, 8765, -3481, 70, -5432];
+// transactions.forEach(function(transaction, idx, arr) {
+//   if(transaction > 0) {
+//     console.log( `Transaction ${idx + 1}: You deposited ${transaction}`);
+//   }
+//   else {
+//     console.log( `Transaction ${idx + 1}: You withdrew ${Math.abs(transaction)}`);
+//   }
+// })
+
+
+// currencies.forEach(function(value, key, map) {
+//   console.log(`${key}: ${value}`);
+// })
--- js/script.js
+++ js/script.js
@@ -1,25 +1,68 @@
-const userWelcome = document.querySelector('.salute');
-const transactions = [200, 450, -600, 8765, -3481, 70, -5432];
 const currencies = new Map([
   ['USD', 'American dolar'],
   ['CAD', 'Canadian dolar'],
   ['MX', 'Mexican peso'],
 ]);
 
+//DATA
+const user1 = {
+  name: 'Luke skywalker',
+  transactions: [234, 65456, -987, 234, -6543, 78],
+  interestRate: 1.1,
+  pin: 9090
+};
 
-//Lecture
+const user2 = {
+  name: 'Anakin skywalker',
+  transactions: [734, -5459, 487, 9834, 43, 81],
+  interestRate: 1.8,
+  pin: 6666
+};
 
-// transactions.forEach(function(transaction, idx, arr) {
-//   if(transaction > 0) {
-//     console.log( `Transaction ${idx + 1}: You deposited ${transaction}`);
-//   }
-//   else {
-//     console.log( `Transaction ${idx + 1}: You withdrew ${Math.abs(transaction)}`);
-//   }
-// })
+const user3 = {
+  name: 'Han Solo',
+  transactions: [-999, -765, -1987, 23, -6543, 18],
+  interestRate: 2.1,
+  pin: 6677
+};
+
+const user4 = {
+  name: 'Leia organa',
+  transactions: [2222, 4709, -87, -24, 6598, 65],
+  interestRate: 1.0,
+  pin: 5312
+};
+
+const user5 = {
+  name: 'Rey Skywalker',
+  transactions: [34, 9888, -127, 134, -63, 708],
+  interestRate: 1.4,
+  pin: 8760
+};
+
+let accounts = [user1, user2, user3, user4, user5];
+
+//DOM ELEMENTS
+const userWelcome = document.querySelector('.salute');
+const main = document.querySelector('.main');
+const containerTransactions = document.querySelector('.transactions');
 
 
-// currencies.forEach(function(value, key, map) {
-//   console.log(`${key}: ${value}`);
-// })
+const displayTransactions = function(transactions) {
+  containerTransactions.innerHTML = '';
+
+  transactions.forEach(function(tran, idx) {
+    const type = tran > 0 ? 'deposit' : 'withdrawal'
+    const html = `
+    <div class="transaction-row">
+    <div class="transaction-date"> 2 days ago</div>
+      <div class="transaction-place">${idx + 1}</div>
+      <div class="transaction-value value--${type}">$ ${tran}</div>
+    </div>
+    `;
+    containerTransactions.insertAdjacentHTML('afterbegin', html);
+  });
+}
+
+displayTransactions(user1.transactions);
 
